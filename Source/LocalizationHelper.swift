c55f8a1710ad8d3a6a93ee938e5d852879594588
Marat Saytakov <remarr@gmail.com>
2016-04-25 23:22:52 +0300

    clean up

--- Source/LocalizationHelper.swift
+++ Source/LocalizationHelper.swift
@@ -9,14 +9,17 @@
 import Foundation
 
 class LocalizationHelper{
-    class func localize(key:String,count:Int?=nil)->String{
-        let bundlePath = (NSBundle(forClass: LocalizationHelper.self).resourcePath! as NSString).stringByAppendingPathComponent("RelativeFormatter.bundle")
-        
-        var localizedString = NSLocalizedString(key, tableName: "RelativeFormatter", bundle: NSBundle(path: bundlePath)!, value: "", comment: "")
-        
-        if let count = count{
-            localizedString = String.localizedStringWithFormat(localizedString, count)
-        }
-        return localizedString
+
+  class func localize(key:String, count:Int? = nil) -> String {
+    let bundlePath = (NSBundle(forClass: LocalizationHelper.self).resourcePath! as NSString).stringByAppendingPathComponent("RelativeFormatter.bundle")
+
+    var localizedString = NSLocalizedString(key, tableName: "RelativeFormatter", bundle: NSBundle(path: bundlePath)!, value: "", comment: "")
+
+    if let count = count{
+      localizedString = String.localizedStringWithFormat(localizedString, count)
     }
+
+    return localizedString
+  }
+
 }
